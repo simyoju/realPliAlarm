392be76c215b87a47e67e62b9d70647b0ba3359e
simyoju <dawnofspring@daum.net>
2021-07-30 00:24:04 +0900

    Initial Commit

---
+++ realPliAlarm/AddAlarmVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddAlarmVC.swift
+//  realPliAlarm
+//
+//  Created by simyo on 2021/07/30.
+//
+
+import UIKit
+
+class AddAlarmVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
